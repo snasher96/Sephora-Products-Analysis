ee461ab64bd93261699dfc9665bc92437d5c24ac
Sheikh Nasher <36981589+snasher96@users.noreply.github.com>
2018-07-27 20:37:06 -0400

    Update AnalysisCode.py

--- AnalysisCode.py
+++ AnalysisCode.py
@@ -1,6 +1,16 @@
+"""
+Creater: Sheikh "Nash" Nasher
+Date Created: April 1st, 2018
+"""
 
-The CSV file named "Products" contains the information about 28,000+ products sold on Sephora website. About 20,000 of them are discontinued (disregard them). For the remaining products, calculate the mean number of reviews for the 10% cheapest products (the bottom decile), the next eight deciles, and for the top 10% most expensive products (the top decile). Plot the numbers against the price deciles. The plot shall use dots but no lines. The plot shall have the grid and the axes labels, but no legend. The title and the X axis ticks are optional. Save the figure as a PNG file with the resolution of 150dpi.
-
+"""
+The CSV file named "Products" contains the information about 28,000+ products sold on Sephora website. 
+About 20,000 of them are discontinued (disregard them). For the remaining products, calculate the mean 
+number of reviews for the 10% cheapest products (the bottom decile), the next eight deciles, and for the 
+top 10% most expensive products (the top decile). Plot the numbers against the price deciles. The plot 
+shall use dots but no lines. The plot shall have the grid and the axes labels, but no legend. The title 
+and the X axis ticks are optional. Save the figure as a PNG file with the resolution of 150dpi.
+"""
 import pandas as pd
 from matplotlib import pyplot as plt
 
